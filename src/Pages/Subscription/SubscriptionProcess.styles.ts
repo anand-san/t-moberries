6d930b1a594957aeb93c3ea57925e643a530654f
Anand <seanjin17@gmail.com>
2021-06-04 17:28:31 +0530

    add root styles

---
+++ src/Pages/Subscription/SubscriptionProcess.styles.ts
@@ -0,0 +1,10 @@
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      margin: '50px',
+      padding: 30
+    }
+  }),
+);
