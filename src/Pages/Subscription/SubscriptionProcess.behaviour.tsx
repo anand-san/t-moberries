7d23479795ad28d2d2e3f2636ea01b4c80d3d575
Anand <seanjin17@gmail.com>
2021-06-04 21:29:00 +0530

    add api call, add function to generate final price

--- src/Pages/Subscription/SubscriptionProcess.behaviour.tsx
+++ src/Pages/Subscription/SubscriptionProcess.behaviour.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { FormData } from "./SubscriptionProcess.types";
+import { getSubscriptionPrices } from "../../API/request-layer";
+import {SubscriptionPricesResponse} from "../../API/response-types"
 
 export default function SubscriptionProcessBehaviour() {
+  const [isError, setIsError] = React.useState<string | null>(null);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [subscriptionPrices, setSubscriptionPrices] = React.useState<SubscriptionPricesResponse[]>([]);
   const [formData, setFormData] = React.useState<FormData>({
     Duration: 12,
     Amount: 5,
@@ -12,5 +17,25 @@ export default function SubscriptionProcessBehaviour() {
     focused: "name",
     name: "",
   });
-  return { formData, setFormData };
+
+  React.useEffect(() => {
+    getSubscriptionPrices()
+      .then((prices: SubscriptionPricesResponse[]) => {
+        setSubscriptionPrices(prices);
+      })
+      .catch(setIsError)
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [setIsLoading, setIsError]);
+
+  const getFinalPrice = (duration: number, amount: number, pricing: SubscriptionPricesResponse[]) => {
+    return pricing.reduce((accumulator:number, current: SubscriptionPricesResponse) => {
+        if(current.duration_months === duration)
+          accumulator = current.price_usd_per_gb * amount
+        return accumulator
+    }, 0)
+  }
+
+  return { formData, setFormData, isError, isLoading,subscriptionPrices, getFinalPrice };
 }
