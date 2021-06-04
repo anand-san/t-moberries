5b2e136d20b21903538848578481fa26bb775269
Anand <seanjin17@gmail.com>
2021-06-04 19:06:44 +0530

    define formdata and form context types

---
+++ src/Pages/Subscription/SubscriptionProcess.types.ts
@@ -0,0 +1,17 @@
+import { Focused } from 'react-credit-cards'
+
+export interface FormData {
+    Duration: number,
+    Amount: number,
+    UpfrontPayment: boolean,
+    number: number | string,
+    expiry: number | string,
+    cvc: number | string,
+    focused: Focused | string,
+    name: string
+}
+
+export interface FormContext {
+    formData: FormData,
+    setFormData: React.Dispatch<React.SetStateAction<FormData>>
+}
