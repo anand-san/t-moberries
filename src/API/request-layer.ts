d08a0d308d98f03066cc82038b67dbcd59a25251
Anand <seanjin17@gmail.com>
2021-06-04 21:55:16 +0530

    add post request to httpbin function

--- src/API/request-layer.ts
+++ src/API/request-layer.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {FormData} from "../Pages/Subscription/SubscriptionProcess.types"
 const AXIOS_TIMEOUT = 60000;
 
 export let api = axios.create({
@@ -10,4 +11,15 @@ export const getSubscriptionPrices = () => {
   return api.get("https://cloud-storage-prices-moberries.herokuapp.com/prices").then(response => {
     return response.data.subscription_plans
   }).catch(e => { throw e })
+}
+
+export const postData = (payload:FormData) => {
+  return api({
+    baseURL: "https://httpbin.org/",
+    method: "post",
+    url: "post",
+    data: payload,
+  }).then(response => {
+    return response
+  }).catch(e => {throw e})
 }
