509910d573d0a37131f43d332e1fd514e6c5ce5a
Anand <seanjin17@gmail.com>
2021-06-04 22:45:01 +0530

    add steps callback handler

--- src/Components/Stepper/Stepper.behaviour.ts
+++ src/Components/Stepper/Stepper.behaviour.ts
@@ -1,26 +1,37 @@
 import React from 'react'
-import {ComponentProps} from "./Stepper.types"
+import { ComponentProps } from "./Stepper.types"
+import {mainStyle as useStyles} from "./Stepper.styles"
 
 export default function StepperBehaviours(props: ComponentProps) {
-    const [activeStep, setActiveStep] = React.useState(0);
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [processingCallback, setProcessingCallback] = React.useState(false)
+  function getStepContent(step: number) {
+    return props.stepsData[props.stepsLabel[step]].component;
+  }
 
-    function getStepContent(step: number) {
-        return props.stepsData[props.stepsLabel[step]].component;
-      }
-    
-      const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-      };
-    
-      const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-      };
+  const handleNext = () => {
+    setProcessingCallback(true)
+    let cb = props.stepsData[props.stepsLabel[activeStep]].callback()
+    setTimeout(() => {
+      setProcessingCallback(false)
+      if(!cb)
+        return
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    }, 350)
+  };
 
-    return {
-        activeStep,
-        setActiveStep,
-        getStepContent,
-        handleNext,
-        handleBack
-    }
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  return {
+    activeStep,
+    setActiveStep,
+    getStepContent,
+    handleNext,
+    handleBack,
+    processingCallback,
+    classes
+  }
 }
