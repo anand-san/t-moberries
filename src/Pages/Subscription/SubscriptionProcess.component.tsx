4d824a5f97251b2228a930eaa74db6e8936e25e9
Anand <seanjin17@gmail.com>
2021-06-04 21:29:25 +0530

    create footer element and pass it into stepper

--- src/Pages/Subscription/SubscriptionProcess.component.tsx
+++ src/Pages/Subscription/SubscriptionProcess.component.tsx
@@ -8,7 +8,6 @@ import GroupAddIcon from "@material-ui/icons/GroupAdd";
 import VideoLabelIcon from "@material-ui/icons/VideoLabel";
 import { SubscriptionFormContext } from "../../Utility/useAppContextHook";
 import SubscriptionProcessBehaviour from "./SubscriptionProcess.behaviour";
-
 const stepsData = {
   "Select Subscription Parameters": {
     icon: <SettingsIcon />,
@@ -26,16 +25,24 @@ const stepsData = {
 
 const stepsLabel = Object.keys(stepsData);
 
+
+
 function App() {
   const classes = useStyles();
-  const { formData, setFormData } = SubscriptionProcessBehaviour();
+  const { formData, setFormData, isLoading, subscriptionPrices, getFinalPrice } = SubscriptionProcessBehaviour();
+
+  const footerLeft = (
+    <div className={classes.footerLeft}>
+      <span>Selected Plan: {`${formData.Duration} Months/${formData.Amount} GB`}</span>
+      <span>Final Price: {`${isLoading ? "Fetching" : "$" + getFinalPrice(formData.Duration, formData.Amount, subscriptionPrices)}`}</span>
+    </div>
+  );
+
   return (
     <Container maxWidth="md">
       <Paper elevation={10} className={classes.root}>
-        <SubscriptionFormContext.Provider
-          value={{ formData, setFormData }}
-        >
-          <Stepper stepsData={stepsData} stepsLabel={stepsLabel} />
+        <SubscriptionFormContext.Provider value={{ formData, setFormData }}>
+          <Stepper stepsData={stepsData} stepsLabel={stepsLabel} footerLeft={footerLeft} />
         </SubscriptionFormContext.Provider>
       </Paper>
     </Container>
