ddf4020b473d7903b6390c529312a7354186cd09
Anand <seanjin17@gmail.com>
2021-06-04 19:07:47 +0530

    wrap child components into context provider

--- src/Pages/Subscription/SubscriptionProcess.component.tsx
+++ src/Pages/Subscription/SubscriptionProcess.component.tsx
@@ -4,31 +4,40 @@ import * as Steps from "./Steps";
 import Paper from "@material-ui/core/Paper";
 import { useStyles } from "./SubscriptionProcess.styles";
 import { Container } from "@material-ui/core";
-import SettingsIcon from '@material-ui/icons/Settings';
-import GroupAddIcon from '@material-ui/icons/GroupAdd';
-import VideoLabelIcon from '@material-ui/icons/VideoLabel';
+import SettingsIcon from "@material-ui/icons/Settings";
+import GroupAddIcon from "@material-ui/icons/GroupAdd";
+import VideoLabelIcon from "@material-ui/icons/VideoLabel";
+import { SubscriptionFormContext } from "../../Utility/useAppContextHook";
+import SubscriptionProcessBehaviour from "./SubscriptionProcess.behaviour";
+
 const stepsData = {
   "Select Subscription Parameters": {
     icon: <SettingsIcon />,
-    component: <Steps.Parameters />
+    component: <Steps.Parameters />,
   },
-  "Payment": {
+  Payment: {
     icon: <GroupAddIcon />,
-    component: <Steps.PaymentData />
+    component: <Steps.PaymentData />,
   },
-  "Confirmation": {
+  Confirmation: {
     icon: <VideoLabelIcon />,
-    component: <Steps.Confirmation />
-  }}
+    component: <Steps.Confirmation />,
+  },
+};
 
-const stepsLabel = Object.keys(stepsData)
+const stepsLabel = Object.keys(stepsData);
 
 function App() {
   const classes = useStyles();
+  const { formData, setFormData } = SubscriptionProcessBehaviour();
   return (
     <Container maxWidth="md">
       <Paper elevation={10} className={classes.root}>
-        <Stepper stepsData={stepsData} stepsLabel={stepsLabel}/>
+        <SubscriptionFormContext.Provider
+          value={{ formData, setFormData }}
+        >
+          <Stepper stepsData={stepsData} stepsLabel={stepsLabel} />
+        </SubscriptionFormContext.Provider>
       </Paper>
     </Container>
   );
