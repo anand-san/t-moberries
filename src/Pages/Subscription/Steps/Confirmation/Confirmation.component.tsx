bace5464ac9024482ea9eaca6bbb42fe0b57f347
Anand <seanjin17@gmail.com>
2021-06-04 21:54:18 +0530

    create confirmation step component

--- src/Pages/Subscription/Steps/Confirmation/Confirmation.component.tsx
+++ src/Pages/Subscription/Steps/Confirmation/Confirmation.component.tsx
@@ -1,7 +1,35 @@
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import PaymentDataBehaviour from "./Confirmation.behaviour";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+
 export default function Confirmation() {
-    return (
-        <div>
-            Confirmation
-        </div>
-    )
+  const { handleChange, formData, handleTnC } = PaymentDataBehaviour();
+  return (
+    <Grid container spacing={8}>
+      <Grid item xs={12}>
+        <TextField
+          label="Email Address"
+          name="UserEmail"
+          variant="outlined"
+          value={formData?.UserEmail}
+          onChange={handleChange}
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={formData?.TnC}
+              onChange={handleTnC}
+              name="TnC"
+            />
+          }
+          label="I agree with the Terms and Conditions"
+        />
+      </Grid>
+    </Grid>
+  );
 }
