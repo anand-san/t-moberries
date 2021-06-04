65134c91517a023ac36989e66405e700262198b9
Anand <seanjin17@gmail.com>
2021-06-04 22:46:22 +0530

    mvoe steps data arr into behaviour file

--- src/Pages/Subscription/SubscriptionProcess.behaviour.tsx
+++ src/Pages/Subscription/SubscriptionProcess.behaviour.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { FormData } from "./SubscriptionProcess.types";
 import { getSubscriptionPrices } from "../../API/request-layer";
 import {SubscriptionPricesResponse} from "../../API/response-types"
+import SettingsIcon from "@material-ui/icons/Settings";
+import GroupAddIcon from "@material-ui/icons/GroupAdd";
+import VideoLabelIcon from "@material-ui/icons/VideoLabel";
+import { useStyles } from "./SubscriptionProcess.styles";
+import * as Steps from "./Steps";
 
 export default function SubscriptionProcessBehaviour() {
   const [isError, setIsError] = React.useState<string | null>(null);
@@ -19,6 +24,8 @@ export default function SubscriptionProcessBehaviour() {
     UserEmail: "",
     TnC: false
   });
+
+  const classes = useStyles();
 
   React.useEffect(() => {
     getSubscriptionPrices()
@@ -39,5 +46,23 @@ export default function SubscriptionProcessBehaviour() {
     }, 0)
   }
 
-  return { formData, setFormData, isError, isLoading,subscriptionPrices, getFinalPrice };
+  const stepsData = {
+    "Select Subscription Parameters": {
+      icon: <SettingsIcon />,
+      component: <Steps.Parameters />,
+      callback: () => console.log("Step1")
+    },
+    Payment: {
+      icon: <GroupAddIcon />,
+      component: <Steps.PaymentData />,
+      callback: () => console.log("Step2")
+    },
+    Confirmation: {
+      icon: <VideoLabelIcon />,
+      component: <Steps.Confirmation />,
+      callback: () => console.log("Step3")
+    },
+  };
+
+  return { formData, setFormData, isError, isLoading,subscriptionPrices, getFinalPrice, stepsData, classes };
 }
--- src/Pages/Subscription/SubscriptionProcess.component.tsx
+++ src/Pages/Subscription/SubscriptionProcess.component.tsx
@@ -1,35 +1,11 @@
 import Stepper from "../../Components/Stepper";
-import * as Steps from "./Steps";
 import Paper from "@material-ui/core/Paper";
-import { useStyles } from "./SubscriptionProcess.styles";
 import { Container } from "@material-ui/core";
-import SettingsIcon from "@material-ui/icons/Settings";
-import GroupAddIcon from "@material-ui/icons/GroupAdd";
-import VideoLabelIcon from "@material-ui/icons/VideoLabel";
 import { SubscriptionFormContext } from "../../Utility/useAppContextHook";
 import SubscriptionProcessBehaviour from "./SubscriptionProcess.behaviour";
-const stepsData = {
-  "Select Subscription Parameters": {
-    icon: <SettingsIcon />,
-    component: <Steps.Parameters />,
-  },
-  Payment: {
-    icon: <GroupAddIcon />,
-    component: <Steps.PaymentData />,
-  },
-  Confirmation: {
-    icon: <VideoLabelIcon />,
-    component: <Steps.Confirmation />,
-  },
-};
-
-const stepsLabel = Object.keys(stepsData);
-
-
 
 function App() {
-  const classes = useStyles();
-  const { formData, setFormData, isLoading, subscriptionPrices, getFinalPrice } = SubscriptionProcessBehaviour();
+  const { formData, setFormData, isLoading, subscriptionPrices, getFinalPrice, stepsData, classes } = SubscriptionProcessBehaviour();
 
   const footerLeft = (
     <div className={classes.footerLeft}>
@@ -42,7 +18,7 @@ function App() {
     <Container maxWidth="md">
       <Paper elevation={10} className={classes.root}>
         <SubscriptionFormContext.Provider value={{ formData, setFormData }}>
-          <Stepper stepsData={stepsData} stepsLabel={stepsLabel} footerLeft={footerLeft} />
+          <Stepper stepsData={stepsData} stepsLabel={Object.keys(stepsData)} footerLeft={footerLeft} />
         </SubscriptionFormContext.Provider>
       </Paper>
     </Container>
