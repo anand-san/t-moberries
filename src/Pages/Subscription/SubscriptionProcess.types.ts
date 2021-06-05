f55cecea8f1fcd6b06df9115f80ba5247e8ffe7e
Anand <seanjin17@gmail.com>
2021-06-05 13:35:49 +0530

    converted formData interface to allow dynamic keys

--- src/Pages/Subscription/SubscriptionProcess.types.ts
+++ src/Pages/Subscription/SubscriptionProcess.types.ts
@@ -1,16 +1,11 @@
-import { Focused } from 'react-credit-cards'
+interface FormDataElements {
+    Value: string | number | boolean,
+    Error?: boolean,
+    HelperText?: string
+}
 
 export interface FormData {
-    Duration: number,
-    Amount: number,
-    UpfrontPayment: boolean,
-    number: number | string,
-    expiry: number | string,
-    cvc: number | string,
-    focused: Focused | string,
-    name: string,
-    UserEmail: string,
-    TnC: boolean
+    [key : string]: FormDataElements
 }
 
 export interface FormContext {
